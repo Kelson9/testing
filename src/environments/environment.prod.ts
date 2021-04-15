1f24c5ed91e2f0c4d0f66623f9724460a26fbf3a
Kelson9 <kelsonngoe@gmail.com>
2021-04-15 10:24:18 -1100

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDR2bL1f79D5T3GK6044knR-3Ri_A261Y4",
+    authDomain: "asend-185ba.firebaseapp.com",
+    databaseURL: "https://asend-185ba-default-rtdb.firebaseio.com",
+    projectId: "asend-185ba",
+    storageBucket: "asend-185ba.appspot.com",
+    messagingSenderId: "451800269368",
+    appId: "1:451800269368:web:c869394fbd933e49dd1ccd",
+    measurementId: "G-QHN9CEGJXZ"
+  }
 };
